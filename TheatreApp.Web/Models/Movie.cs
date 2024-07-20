7c1b11a2233fb3a4b7609bad916dbdd193413b73
Revan <x.reev3350@gmail.com>
2024-07-20 20:40:09 +0300

    added Validations

--- TheatreApp.Web/Models/Movie.cs
+++ TheatreApp.Web/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheaterApp.Models
@@ -6,8 +7,12 @@ namespace TheaterApp.Models
     public class Movie
     {
         public int MovieID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(20)]
         public string MovieTitle { get; set; }
         public float ImdbRating { get; set; }
+        [Required]
         public int YearReleased { get; set; }
         public decimal Budget { get; set; }
         public decimal BoxOffice { get; set; }
